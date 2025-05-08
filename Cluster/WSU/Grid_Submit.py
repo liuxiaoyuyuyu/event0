f13ad8e6a10c58a0ed3d2462a0d3b2be7eea9c14
Xiaoyu Liu <xl155@rice.edu>
2025-05-08 14:32:48 +0200

    update

--- Cluster/WSU/Grid_Submit.py
+++ Cluster/WSU/Grid_Submit.py
@@ -4,14 +4,16 @@
 
 from subprocess import call
 import sys
-def submit(fold_id=0,nevent = 10):
+import random
+import time
+def submit(fold_id_start=0, fold_id_end = 1, nevent = 10, random_number = 0):
     jobs = '''#!/bin/bash
-#SBATCH --job-name SoftHard{fold_id}
-#SBATCH -t 23:59:00
+#SBATCH --job-name SoftZPC{fold_id_start}
+#SBATCH -t 16:59:00
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=2gb
+#SBATCH --mem-per-cpu=3gb
 #SBATCH --constraint="intel"
 #SBATCH --mail-type=all
 #SBATCH --mail-user=XXXX@wayne.edu
@@ -72,36 +74,45 @@ date
 #cd ../
 
 # Then run the framework
+for (( ii={fold_id_start}; ii<{fold_id_end}; ii++ ))
+do
 
-cp -r event0 Playground/job-{fold_id}
-cd Playground/job-{fold_id}
+cp -r event0 PlaygroundZPC/job-$ii
+cd PlaygroundZPC/job-$ii
 
 # Generate the pythia parton
+#sleep 1s
 cd pythia_parton
-./mymain06 {nevent}
+./mymain06 {nevent} $(({random_number} + $ii * 12345))
 cd ../
 
 # ZPC for parton cascade
 cd  ZPC
 ln -s ../pythia_parton/parton_info.dat ./
 ./exec
+rm -r ana/parton-collisionsHistory.dat
+rm -r ana/zpc.res
 cd ../
+rm -rf pythia_parton/parton_info.dat
 
 # fragmentation and urqmd
 cd hadronization_urqmd
 cd fragmentation
 ln -s ../../ZPC/ana/zpc.dat ./
 ./main_string_fragmentation {nevent}
+rm -r ../../ZPC/ana/*
 
 cd ../urqmd_code
     # script to run urqmd
     cd osc2u
     ln -s ../../fragmentation/hadrons_frag1.dat ./
     ./osc2u.e < hadrons_frag1.dat > run.log
+    rm -r ../../fragmentation/hadrons_frag1.dat
     mv fort.14 ../urqmd/OSCAR.input
     cd ../urqmd
     ./runqmd.sh > run.log
     rm -fr OSCAR.input
+    rm -rf run.log
     cd ..
 cd ../
 cd ../
@@ -109,15 +120,21 @@ cd ../
 cd fastjet_hadron
 ln -s ../hadronization_urqmd/urqmd_code/urqmd/particle_list.dat ./
 ./fastjet_hadron {nevent}
+rm -r ../hadronization_urqmd/urqmd_code/urqmd/particle_list.dat
 cd ../
 
 # Save the final results into folder
 mkdir results
-mv fastjet_hadron/final_state_hard_hadrons.bin ./results/{fold_id}.bin
+mv fastjet_hadron/final_state_hard_hadrons.bin ./results/$ii.bin
+rm -r fastjet_hadron
+rm -r hadronization_urqmd 
+rm -r pythia_parton 
+rm -r ZPC 
+cd ../../
+done
+'''.format(fold_id_start=fold_id_start, fold_id_end = fold_id_end, nevent = nevent, random_number = random_number)
 
-'''.format(fold_id=fold_id, nevent = nevent)
-
-    job_name = "NSC3_%s.sh"%(fold_id)
+    job_name = "NSC3_%s.sh"%(fold_id_start)
     with open(job_name, 'w') as fout:
         fout.write(jobs)
     call(['sbatch', job_name])
@@ -127,10 +144,19 @@ mv fastjet_hadron/final_state_hard_hadrons.bin ./results/{fold_id}.bin
 
 if __name__=='__main__':
     import sys
-    fold_id = int(int(sys.argv[1]))
-    nevent = int(int(sys.argv[2]))
+    fold_id_start = int(int(sys.argv[1]))
+    nfold = int(int(sys.argv[2]))
+    nevent = int(int(sys.argv[3]))
+    # Get the current system time as a seed
+    seed = int(time.time())
+
+    # Seed the random number generator
+    random.seed(seed)
+
+    # Generate a random number between 0 and 10,000,000
+    random_number = random.randint(0, 10**8)
     #for n in range(0,nods):
-    #mmid = fold_id * tot_ev + n 
-    submit(fold_id,nevent)
+    #mmid = fold_id_start * tot_ev + n 
+    submit(fold_id_start * nfold, nfold + fold_id_start * nfold, nevent, random_number + fold_id_start)
     #start_num += jobs_per_cpu 
 
--- scripts/Calculate_flow/Combine_Nch.py
+++ scripts/Calculate_flow/Combine_Nch.py
@@ -6,24 +6,14 @@ import re
 import math
 import numpy as np
 import os
-import sys
 
-playground = os.listdir("../../../Playground")
-QAQBall = np.zeros((11))
-Neventall = np.zeros((11))
-user_input = int(sys.argv[1])
-for ifolder in range(user_input, user_input + 1):
+playground = os.listdir("./Playground")
+QAQBall = 0
+for ifolder in range(50):
     print(ifolder)
-    folderpath = os.listdir("../../../Playground/job-{}/results/Nch".format(ifolder))
-    for ifile in range(len(folderpath)):
-        QAQBtemp = np.loadtxt("../../../Playground/job-{}/results/Nch/{}".format(ifolder, folderpath[ifile]))
-        for ii in range(len(QAQBtemp)):
-            nchb = int(QAQBtemp[ii]/10)
-            if (nchb >10):
-                nchb = 10
-            QAQBall[nchb] = QAQBtemp[ii] + QAQBall[nchb]
-            Neventall[nchb] = Neventall[nchb] +1
-aaa = np.array([Neventall, QAQBall])
-np.savetxt("Nch_{}".format(user_input), aaa.T)
-
+    #folderpath = os.listdir("../Playground/job-{}/results/QAQiB".format(ifolder))
+    #for ifile in range(len(folderpath)):
+    QAQBtemp = np.loadtxt("./Playground/job-{}/Nch_{}".format(ifolder, ifolder))
+    QAQBall = QAQBtemp + QAQBall
+np.savetxt("Nchall", QAQBall)
 
